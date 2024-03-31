e2d80338ea063eb2ed961ae42bf66cdc02c8c4af
Dhirajjjj <patildhiraj2306@gmail.com>
2024-03-31 16:47:45 +0530

    dynamic serach - WIP

---
+++ Frontend/src/components/constants/searchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function SearchBar({ value, onChange }) {
+    return (
+        <input
+            onChange={onChange}
+            value={value}
+            type="text"
+            placeholder="Search"
+            class="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" 
+        />
+    );
+}
+
+export default SearchBar;
